4c7e9bb300a3a80f206aa997deaf157b4b1cceb8
Ed Summers <ehs@pobox.com>
2021-06-27 21:47:40 -0400

    need to bundle index.html

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setuptools.setup(
     name='twarc-network',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/docnow/twarc-network',
     author='Ed Summers',
     author_email='ehs@pobox.com',
